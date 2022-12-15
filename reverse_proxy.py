f2b460fb9bc71c661561ab389b3e4bc89c4a64f1
Enache Andrei <enache.andrei.george@gmail.com>
2022-12-15 23:07:01 +0200

    modified yaml config file structure, modified tcp_healthcheck function, added exceptions

--- reverse_proxy.py
+++ reverse_proxy.py
@@ -1,3 +1,4 @@
+import random
 import time
 from http.server import HTTPServer, BaseHTTPRequestHandler, ThreadingHTTPServer
 from random import randrange
@@ -6,10 +7,11 @@ import socket
 import socketserver
 import requests
 import yaml
+import threading
 
-HOST_NAME = socket.gethostname()
-HOST_IP = socket.gethostbyname(HOST_NAME)
-# HOST_IP = "127.0.0.1"
+# HOST_NAME = socket.gethostname()
+# HOST_IP = socket.gethostbyname(HOST_NAME)
+HOST_IP = "127.0.0.1"
 PORT = 8081
 ADDR = (HOST_IP, PORT)
 
@@ -19,66 +21,46 @@ CLIENT3 = 'https://httpbin.org'
 CONFIG_PATH = "./config.yaml"
 
 
+def tcp_healthcheck():
+    """
+    interval = check for status each X seconds
+    timeout = when checking the health, try for X seconds, if no response -> unhealthy
+    :return:
+    """
+    threading.Timer(15.0, tcp_healthcheck).start()
+    print('Printed from outside')
+
+
 class ReverseProxy(BaseHTTPRequestHandler):
     server_version = "ReverseProxy/1.1"
     protocol_version = 'HTTP/1.0'
-
+    tcp_healthcheck()
 
     def __init__(self, *args):
         self.config = self.read_yaml(CONFIG_PATH)
         self.services = self.get_services(self.config)
-        self.client_list = self.get_upstream_services(self.services)
+        # self.upstream_srv_list = self.get_upstream_services(self.services)
+        self.upstream_srv_list = self.services['hosts']
+        self.healthy_upstream_srv = self.upstream_srv_list
         self.load_balancer_type = self.services['lbPolicy']
-        self.round_robin_server = cycle(self.client_list)
-        self.connected_users = []
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.settimeout(1000)
+        self.round_robin_server = cycle(self.healthy_upstream_srv)
+        self.sock = self.define_socket()
+        # self.tcp_healthcheck()
         BaseHTTPRequestHandler.__init__(self, *args)
 
     def do_GET(self):
-
-        current_try_count = 0
-        try:
-            self.sock.connect(("127.0.0.1", 8888))
-            print('WOW BE CAREFUL')
-            print(self.sock)
-            # self.sock.settimeout(None)
-        except Exception as e:
-            print('EROARE BOSSSS')
-            print(self.sock)
-            print(e)
-        # self.sock.sendall("GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n")
-        # try:
-        #     self.sock.connect(('123.123.123.123', 12345))  # "random" IP address and port
-        # except socket.error as exc:
-        #     print("Erreeeeeeeeeee", "Caught exception socket.error : %s" % exc)
-        # try:
-        #     print('Hiaaa')
-        #     current_try_count += 1
-        #     self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        #     self.sock.bind(("127.0.0.1", 8888))
-        #     self.sock.listen(1)
-        # except socket.error:
-        #     print("Try XXX")
-        #     time.sleep(1)
-        #     self.sock.listen(1)
-        # except Exception:
-        #     print("Unable to start")
-
         # url = f"{CLIENT3}/get"
-        # # url = self.select_upstream_service_with_lb(self.load_balancer_type)
-        #
-        # print(self.headers)
-        # req_header = self.parse_headers()  # header from client
-        # resp = requests.get(url,
-        #                     headers=req_header,
-        #                     verify=False)  # we pass the header from client to the upstream service and get the response
-        #
-        # self.send_resp_from_upstream(resp)
-        #
-        # self.connected_users.append(self.headers['Host'])
-        # print(self.connected_users)
+        self.round_robin_server = cycle(self.healthy_upstream_srv)
+        url = self.select_upstream_service_with_lb(self.load_balancer_type)
+        print(self.headers)
+
+        req_header = self.parse_headers()  # header from client
+        resp = requests.get(url,
+                            headers=req_header,
+                            verify=False)  # we pass the header from client to the upstream service and get the response
+
+        self.send_resp_from_upstream(resp)
+
 
     def do_POST(self):
         url = f"{CLIENT3}/post"
@@ -115,21 +97,58 @@ class ReverseProxy(BaseHTTPRequestHandler):
             self.send_header("Content-length", '0')
         self.end_headers()
 
-    def round_robin(self, iter):
-        return next(iter)
+    def round_robin(self, iterator):
+        return next(iterator)
 
     def select_upstream_service_with_lb(self, lb_policy):
         if lb_policy == "ROUND_ROBIN":
-            curr_upstream_service = self.round_robin(self.round_robin_server)
-            url = f"http://{curr_upstream_service}"
+            upstream_service = self.round_robin(self.round_robin_server)
+            url = f"http://{upstream_service['address']}:{upstream_service['port']}"
+            print('URL FROM LB: ', url)
             return url
         else:
-            curr_upstream_service = self.client_list[randrange(2)]
-            url = f"http://{curr_upstream_service}"
+            upstream_service = self.upstream_srv_list[randrange(2)]
+            url = f"http://{upstream_service['address']}:{upstream_service['port']}"
             return url
 
+    def define_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return sock
+
     def tcp_healthcheck(self):
-        pass
+        """
+        interval = check for status each X seconds
+        timeout = when checking the health, try for X seconds, if no response -> unhealthy
+        :return:
+        """
+        healthcheck_params = self.services['tcpHealthcheck'][0]
+        threading.Timer(healthcheck_params['interval'], self.tcp_healthcheck).start()
+        self.healthy_upstream_srv = []
+        for service in self.upstream_srv_list:
+            print('Curr service: ', service)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.settimeout(healthcheck_params['timeout'])
+            # try:
+            #     sock.connect((service['address'], service['port']))
+                # sock.close()
+                # self.healthy_upstream_srv.append(service)
+                # print('Ok service: ', service)
+            # except TimeoutError:
+            #     print('Server has timed out, please try again later.')
+            #     print('NOT Ok service: ', service)
+            # except socket.error as e:
+            #     print('Socket error: ', e)
+            #     print('NOT Ok service: ', service)
+            # except Exception as e:
+            #     print('NOT Ok service: ', service)
+            #     print('Another exception occured: ', e)
+        # print('Healthy upstream: ', self.healthy_upstream_srv)
+
+    @staticmethod
+    def retry_policy():
+        print('AAAAAAAAAAAAAAA')
 
     def read_yaml(self, config_path):
         with open(config_path, "r") as f:
@@ -146,18 +165,18 @@ class ReverseProxy(BaseHTTPRequestHandler):
         return services_dict
 
     def get_upstream_services(self, services_dict):
-        # print(services_dict)
-        upstream_services_list = []
+        print(services_dict)
+        upstream_hosts_list = []
         hosts_list = services_dict["hosts"]
         for host in hosts_list:
-            upstream_services_list.append(f"{host['address']}:{host['port']}")
-        # print(upstream_services_list)
-        return upstream_services_list
+            upstream_hosts_list.append(f"{host['address']}:{host['port']}")
+        # print(upstream_hosts_list)
+        return upstream_hosts_list
 
 
 def main(handler_class=ReverseProxy, server_address=ADDR):
-    print('http server is starting on {} port {}...'.format(HOST_IP, PORT))
-    web_server = ThreadingHTTPServer(server_address, handler_class)
+    print('http server is starting on {}:{}...'.format(HOST_IP, PORT))
+    web_server = ThreadingHTTPServer(server_address, ReverseProxy)
     print('http server is running as reverse proxy')
     try:
         web_server.serve_forever()
