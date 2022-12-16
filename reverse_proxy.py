28e30f69904d51308f3418b61bf5a7401a4b0284
Enache Andrei <enache.andrei.george@gmail.com>
2022-12-16 18:11:51 +0200

    Patched load balancer and tcphealthcheck

--- reverse_proxy.py
+++ reverse_proxy.py
@@ -21,45 +21,108 @@ CLIENT3 = 'https://httpbin.org'
 CONFIG_PATH = "./config.yaml"
 
 
-def tcp_healthcheck():
+def read_yaml(config_path):
+    with open(config_path, "r") as f:
+        try:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            print(config)
+            return config
+        except yaml.YAMLError as exc:
+            print(exc)
+            return
+
+
+def get_services(config):
+    services_dict = config['proxy']['services'][0]
+    return services_dict
+
+
+def tcp_healthcheck(healthy_upstr_srv, services):
     """
     interval = check for status each X seconds
     timeout = when checking the health, try for X seconds, if no response -> unhealthy
     :return:
     """
-    threading.Timer(15.0, tcp_healthcheck).start()
-    print('Printed from outside')
+    healthcheck_params = services['tcpHealthcheck'][0]  # interval, timeout
+    upstream_services = services['hosts']  # list of upstream services dictionaries
+    unhealthy_upstr_srv = []
+
+    for service in upstream_services:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(healthcheck_params['timeout'])
+        try:
+            sock.connect((service['address'], service['port']))
+            sock.close()
+            if service not in healthy_upstr_srv:
+                healthy_upstr_srv.append(service)
+            if service in unhealthy_upstr_srv:
+                unhealthy_upstr_srv.remove(service)
+        except TimeoutError:
+            if service in healthy_upstr_srv:
+                healthy_upstr_srv.remove(service)
+            if service not in unhealthy_upstr_srv:
+                unhealthy_upstr_srv.append(service)
+            print(f"Server has timed out, please try again later. Service: {service['address']}:{service['port']}")
+        except socket.error as e:
+            if service in healthy_upstr_srv:
+                healthy_upstr_srv.remove(service)
+            if service not in unhealthy_upstr_srv:
+                unhealthy_upstr_srv.append(service)
+            print('Socket error, service not available: ', e, f"Service: {service['address']}:{service['port']}")
+        except Exception as e:
+            if service in healthy_upstr_srv:
+                healthy_upstr_srv.remove(service)
+            if service not in unhealthy_upstr_srv:
+                unhealthy_upstr_srv.append(service)
+            print('Exception occured, please try again later: ', e)
+    print('Healthy upstream: ', healthy_upstr_srv)
+    print('Unhealthy upstream: ', unhealthy_upstr_srv)
+
+    my_thread = threading.Timer(interval=healthcheck_params['interval'],
+                                function=tcp_healthcheck,
+                                args=(healthy_upstr_srv, services, ))
+    my_thread.daemon = True
+    my_thread.start()
+
 
 
 class ReverseProxy(BaseHTTPRequestHandler):
     server_version = "ReverseProxy/1.1"
     protocol_version = 'HTTP/1.0'
-    tcp_healthcheck()
+    config = read_yaml(CONFIG_PATH)
+    services = get_services(config)
+    upstream_srv_list = services['hosts']
+    healthy_upstream_srv = []
+    # dummy_healthy_srv_list = upstream_srv_list.copy()
+    tcp_healthcheck(healthy_upstream_srv, services)
 
     def __init__(self, *args):
-        self.config = self.read_yaml(CONFIG_PATH)
-        self.services = self.get_services(self.config)
-        # self.upstream_srv_list = self.get_upstream_services(self.services)
-        self.upstream_srv_list = self.services['hosts']
-        self.healthy_upstream_srv = self.upstream_srv_list
+        # self.test = self.healthy_upstream_srv  # this was ok, we can return to this
         self.load_balancer_type = self.services['lbPolicy']
         self.round_robin_server = cycle(self.healthy_upstream_srv)
         self.sock = self.define_socket()
-        # self.tcp_healthcheck()
         BaseHTTPRequestHandler.__init__(self, *args)
 
     def do_GET(self):
         # url = f"{CLIENT3}/get"
-        self.round_robin_server = cycle(self.healthy_upstream_srv)
-        url = self.select_upstream_service_with_lb(self.load_balancer_type)
-        print(self.headers)
+        if not self.healthy_upstream_srv:
+            self.send_response(500)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<p>Sorry, no servers available</p>", "utf-8"))
+        else:
+            # self.round_robin_server = cycle(self.healthy_upstream_srv)
+            url = self.select_upstream_service_with_lb(self.load_balancer_type)
 
-        req_header = self.parse_headers()  # header from client
-        resp = requests.get(url,
-                            headers=req_header,
-                            verify=False)  # we pass the header from client to the upstream service and get the response
+            print('THIS IS FROM GET: ', self.test)
 
-        self.send_resp_from_upstream(resp)
+            req_header = self.parse_headers()  # header from client
+            resp = requests.get(url,
+                                headers=req_header,
+                                verify=False)  # we pass the header from client to the upstream service and get the response
+
+            self.send_resp_from_upstream(resp)
 
 
     def do_POST(self):
@@ -107,7 +170,8 @@ class ReverseProxy(BaseHTTPRequestHandler):
             print('URL FROM LB: ', url)
             return url
         else:
-            upstream_service = self.upstream_srv_list[randrange(2)]
+            num_available_srv = len(self.healthy_upstream_srv)
+            upstream_service = self.healthy_upstream_srv[randrange(num_available_srv)]
             url = f"http://{upstream_service['address']}:{upstream_service['port']}"
             return url
 
@@ -116,67 +180,23 @@ class ReverseProxy(BaseHTTPRequestHandler):
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return sock
 
-    def tcp_healthcheck(self):
-        """
-        interval = check for status each X seconds
-        timeout = when checking the health, try for X seconds, if no response -> unhealthy
-        :return:
-        """
-        healthcheck_params = self.services['tcpHealthcheck'][0]
-        threading.Timer(healthcheck_params['interval'], self.tcp_healthcheck).start()
-        self.healthy_upstream_srv = []
-        for service in self.upstream_srv_list:
-            print('Curr service: ', service)
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.settimeout(healthcheck_params['timeout'])
-            # try:
-            #     sock.connect((service['address'], service['port']))
-                # sock.close()
-                # self.healthy_upstream_srv.append(service)
-                # print('Ok service: ', service)
-            # except TimeoutError:
-            #     print('Server has timed out, please try again later.')
-            #     print('NOT Ok service: ', service)
-            # except socket.error as e:
-            #     print('Socket error: ', e)
-            #     print('NOT Ok service: ', service)
-            # except Exception as e:
-            #     print('NOT Ok service: ', service)
-            #     print('Another exception occured: ', e)
-        # print('Healthy upstream: ', self.healthy_upstream_srv)
-
     @staticmethod
     def retry_policy():
         print('AAAAAAAAAAAAAAA')
 
-    def read_yaml(self, config_path):
-        with open(config_path, "r") as f:
-            try:
-                config = yaml.load(f, Loader=yaml.FullLoader)
-                print(config)
-                return config
-            except yaml.YAMLError as exc:
-                print(exc)
-                return
-
-    def get_services(self, config):
-        services_dict = config['proxy']['services'][0]
-        return services_dict
-
-    def get_upstream_services(self, services_dict):
-        print(services_dict)
-        upstream_hosts_list = []
-        hosts_list = services_dict["hosts"]
-        for host in hosts_list:
-            upstream_hosts_list.append(f"{host['address']}:{host['port']}")
-        # print(upstream_hosts_list)
-        return upstream_hosts_list
+    # def get_upstream_services(self, services_dict):
+    #     print(services_dict)
+    #     upstream_hosts_list = []
+    #     hosts_list = services_dict["hosts"]
+    #     for host in hosts_list:
+    #         upstream_hosts_list.append(f"{host['address']}:{host['port']}")
+    #     print(upstream_hosts_list)
+    #     return upstream_hosts_list
 
 
 def main(handler_class=ReverseProxy, server_address=ADDR):
     print('http server is starting on {}:{}...'.format(HOST_IP, PORT))
-    web_server = ThreadingHTTPServer(server_address, ReverseProxy)
+    web_server = ThreadingHTTPServer(server_address, handler_class)
     print('http server is running as reverse proxy')
     try:
         web_server.serve_forever()
